ae3ac636b5c1f169b09b254f467d43bc589e2172
kalio007 <kalioprince1@gmail.com>
2025-04-21 09:44:05 +0100

    change file structure

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Base } from "./base";
-import { Posts } from "./posts";
-declare class Typicode extends Base {
-}
-interface Typicode extends Posts {
-}
-export default Typicode;
--- dist/posts/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Base } from "../base";
-import { NewPost, Post } from "./types";
-export declare class Posts extends Base {
-    getPostById(id: number): Promise<Post>;
-    getPosts(): Promise<Post[]>;
-    createPost(newPost: NewPost): Promise<Post>;
-}
